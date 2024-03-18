226fd3b970e37187319b46b55879da1df0a78af5
xuanzhi33 <xuanzhi33@qq.com>
2024-03-18 17:18:35 +0800

    Update translation functionality in translate.js

--- translate.js
+++ translate.js
@@ -1,42 +1,46 @@
 /*
 transtale.js
-v1.1
+v1.2
 by xuanzhi33
 */
-function gc() {
-    var name = "transLang=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+window.TranslateJS = (function () {
+    function getTransCookie() {
+        var name = "transLang=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
     }
-    return "";
-}
-function sc(cvalue)
-{
-  var d = new Date();
-  d.setTime(d.getTime()+(30*24*60*60*1000));
-  var expires = "expires="+d.toGMTString();
-  document.cookie = "transLang=" + cvalue + "; " + expires + "; path=/";
-}
-function tr(){
-    $('[en]').each(function(){
-        $(this).html($(this).attr("en"));
-    })
-}
-if (((navigator.language!="zh-CN")||(gc("")=="en"))&&(gc("transLang")!="zh")){
-    tr();
-    $(".transBtn").text("切换语言");
-}else{
-    $(".transBtn").text("Switch language");
-}
-$(".transBtn").click(function(){
-    if ($(".transBtn").text()=="Switch language"){
-        tr();
-        sc("en");
+    function setCookie(cvalue) {
+        var d = new Date();
+        d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = "transLang=" + cvalue + "; " + expires + "; path=/";
+    }
+    function translate() {
+        $('[en]').each(function () {
+            $(this).html($(this).attr("en"));
+        })
+    }
+    if (((navigator.language != "zh-CN") || (getTransCookie() == "en")) && (getTransCookie() != "zh")) {
+        translate();
         $(".transBtn").text("切换语言");
-    }else{
-        sc("zh");
-        location.reload();
+    } else {
+        $(".transBtn").text("Switch language");
     }
-})
+    $(".transBtn").click(function () {
+        if ($(".transBtn").text() == "Switch language") {
+            translate();
+            setCookie("en");
+            $(".transBtn").text("切换语言");
+        } else {
+            setCookie("zh");
+            location.reload();
+        }
+    })
+    return {
+        translate: translate
+    }
+})();
